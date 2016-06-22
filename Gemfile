779101a5f2d2aef1e23ce0a66420c9c6c69c321f
galulex <alex@active-bridge.com>
2016-06-22 23:46:12 +0300

    Add capistrano

--- Gemfile
+++ Gemfile
@@ -30,4 +30,7 @@ group :development do
   gem 'web-console', '~> 2.0'
   gem 'spring'
   gem 'puma'
+  gem 'capistrano'
+  gem 'capistrano-passenger'
+  gem 'capistrano-bundler'
 end
---
+++ config/deploy.rb
@@ -0,0 +1,27 @@
+# config valid only for current version of Capistrano
+lock '3.5.0'
+
+set :application, 'cloud_monitor'
+set :repo_url, 'git@github.com:galulex/monitor_app.git'
+
+set :deploy_to, '/home/ubuntu/cloud_monitor'
+
+set :linked_dirs, fetch(:linked_dirs, []).push(
+  'log',
+  'tmp/pids',
+  'tmp/cache',
+  'tmp/sockets',
+  'vendor/bundle',
+  'public/system'
+)
+
+namespace :stream do
+  desc 'Restart monitor stream'
+  task :restart do
+    on roles :app do
+      execute "ruby #{current_path}/lib/monitor_control.rb restart"
+    end
+  end
+end
+
+after 'deploy:restart', 'stream:restart'
